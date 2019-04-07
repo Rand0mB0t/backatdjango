637e638ef95c7da9315eb4e2816688037f3db353
Rand0mb0t <sumitbhakt99@gmail.com>
2019-04-07 16:01:31 +0530

    Added template - basic version

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Tutorial
 
-# Create your views here.
+
 
 def homepage(request):
-    return HttpResponse("<H1>Hello World !! </h1>")
+    return render(request=request, 
+                  template_name='main/home.html',
+                  context={"tutorials":Tutorial.objects.all()}
+                  )
