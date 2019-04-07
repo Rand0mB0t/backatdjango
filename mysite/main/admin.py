efddfba759a0106221b3347028c3cd16dda18a71
Rand0mb0t <sumitbhakt99@gmail.com>
2019-04-07 15:43:18 +0530

    Added admin and customized it, with editor

--- mysite/main/admin.py
+++ mysite/main/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from .models import Tutorial
+from tinymce.widgets import TinyMCE 
+from django.db import models
 
 # Register your models here.
+
+class TutorialAdmin(admin.ModelAdmin):
+    '''
+        This class is to modify how our model is presented in the 
+        admin interface, we can use fields or fieldsets mutually
+        exclusively to order or group the fields
+    '''
+    # fields = [
+    #     "tutorial_title",
+    #     "tutorial_content",
+    #     "tutorial_published",
+    # ]
+
+    fieldsets = (
+        ("Title/date", {
+            "fields": (
+                "tutorial_title",
+                "tutorial_published",
+            ),
+        }),
+        ("Content", {
+            "fields":(
+                "tutorial_content",
+            )
+        })
+    )
+
+    formfield_overrides = {
+        models.TextField : {
+            'widget' : TinyMCE()
+        }
+    }
+    
+admin.site.register(Tutorial, TutorialAdmin)
